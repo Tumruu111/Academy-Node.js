14637468b2f766184d0fc62df2174deef089ea49
BACK-2-15 <back-2-15@Erxess-Mac-mini.local>
2025-11-18 17:37:01 +0800

    update

--- index.js
+++ index.js
@@ -2,54 +2,56 @@ import express from "express";
 import fs from "node:fs/promises";
 
 const app = express();
-
 app.use(express.json());
+
+const usersFile = "users.json";
+
+async function loadUsers() {
+  const data = await fs.readFile(usersFile, "utf-8");
+  return JSON.parse(data);
+}
+
+async function saveUsers(users) {
+  await fs.writeFile(usersFile, JSON.stringify(users, null, 2));
+}
 
 app.get("/get-user/:id", async (req, res) => {
   const { id } = req.params;
-  const users = await fs.readFile("users.json").then(value => {
-    return JSON.parse(value);
-  });
+  const users = await loadUsers();
+  const user = users.find((u) => u.id == id);
 
-  const user = users.find(value => {
-    return value.id == id;
-  });
-
-  if (!user) {
-    return res.status(404).send("User not found");
-  }
-
+  if (!user) return res.status(404).send("User not found");
   res.json(user);
 });
 
 app.get("/get-users", async (req, res) => {
-  const { firstName, age } = req.query;
-
-  const users = await fs.readFile("users.json").then(value => {
-    return JSON.parse(value);
-  });
-
+  const users = await loadUsers();
   res.json(users);
 });
 
 app.post("/create-user", async (req, res) => {
-  const userData =  { firstName, age } = req.query;
-
-  await fs.writeFile("users.json", userData, "utf-8");
-
-  res.send("Success");
+  const newUser = { id: Date.now().toString(), ...req.body };
+  const users = await loadUsers();
+  users.push(newUser);
+  await saveUsers(users);
+  res.status(201).json({ message: "User created", user: newUser });
 });
 
 app.put("/update-user/:id", async (req, res) => {
-  const updateData = { firstName, age } = req.query;
+  const { id } = req.params;
+  const updateData = req.body;
+  const users = await loadUsers();
+  const userIndex = users.findIndex((u) => u.id === id);
 
-  
+  if (userIndex === -1) return res.status(404).send("User not found");
 
-  await fs.writeFile("users.json", updateData, "utf-8");
+  users[userIndex] = { ...users[userIndex], ...updateData };
+  await saveUsers(users);
 
-  res.send("Success");
+  res.status(200).json({
+    message: "User updated successfully",
+    user: users[userIndex],
+  });
 });
 
-app.listen(3000, () => {
-  console.log("3000");
-});
+app.listen(3000);
