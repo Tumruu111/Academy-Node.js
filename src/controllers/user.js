0a24676ffbe248c11139fa5b31f6f571f0f2eb71
BACK-2-15 <back-2-15@Erxess-Mac-mini.local>
2025-11-24 19:47:22 +0800

    update

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -5,8 +5,15 @@ export const login = (req, res) => {
   const user = users.find(value => {
     return value.username === username && value.password === password;
   });
-    res.send("success!");
+    res.send("success");
    if (!user) {
     console.log("username eswel password buruu bn!");
    }
+
+   res.cookie("user", user.username, {
+    httpOnly: true,
+    secure: false,
+    sameSite: "None",
+    maxAge: 24 * 60 * 60 * 1000
+   });
 };
--- src/index.js
+++ src/index.js
@@ -2,12 +2,23 @@ import express from "express";
 import { bankRouter } from "./routers/bank.js";
 import { userRouter } from "./routers/user.js";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import path from "path";
 
 const app = express();
 
 app.use(cors());
 
 app.use(express.json()); 
+
+app.use(cookieParser());
+
+app.use("/", (req, res, next) => {
+  const user = req.cookies.user;
+  req.user = user;
+});
+
+app.use(express.static)
 
 app.use("/bank", bankRouter);
 app.use("/user", userRouter);
