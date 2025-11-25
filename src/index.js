35bfbcf70e1e0feb6a4dc0189f5c5d764fa5d676
BACK-2-15 <back-2-15@Erxess-Mac-mini.local>
2025-11-25 17:31:54 +0800

    update

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { userRouters } from "./routers/user.js";
+const app = express();
+
+
+
+app.listen(3000,() => {
+});
---
+++ src/routers/user.js
@@ -0,0 +1,3 @@
+import { Router } from "express";
+
+export const userRouters = new Router();
